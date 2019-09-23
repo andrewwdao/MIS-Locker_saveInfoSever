447f13665c19d75984921e62c9786f537eba2454
Minh An Dao <minhan7497@gmail.com>
2019-09-23 13:34:20 +0700

    add some typing interract into index.html and routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,18 @@ from flask import render_template
 from app import app
 from app.forms import InfoForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = InfoForm()
+    if form.validate_on_submit():
+        flash('Information received:\r\nName: {}\r\nID (MSSV): {}'.format(
+            form.name.data, form.mssv.data))
+        return redirect('/index')
     templateData = {
         'server_title' : 'MIS Locker System',
         'server_func' : 'Add Guest ID',
         'form' : form
-        #'time': timeString
     }
     return render_template('index.html', **templateData)
 
